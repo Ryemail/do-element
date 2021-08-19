f18824d1224902ad1d63a5df35829d96e1045017
Ryemail <1956045780@qq.com>
2021-08-19 18:03:24 +0800

    修改docs  名称

---
+++ document/.vuepress/enhanceApp.js
@@ -0,0 +1,28 @@
+import 'element-ui/lib/theme-chalk/index.css';
+import '../../lib/theme-chalk/index.css';
+import './public/font/iconfont.css';
+
+export default ({ Vue, router }) => {
+	router.beforeEach((to, from, next) => {
+		if (to.path === '/') {
+			next('/zh/component/container');
+		} else if (to.path === '/zh/component/') {
+			next('/zh/component/container');
+		} else if (to.path === '/zh/utils/') {
+			next('/zh/utils/object');
+		} else {
+			next();
+		}
+	});
+	Vue.mixin({
+		mounted() {
+			import('element-ui').then(function (m) {
+				Vue.use(m);
+			});
+
+			import('../../lib').then(function (D) {
+				Vue.use(D);
+			});
+		},
+	});
+};
