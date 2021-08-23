9c69f4a5c8889fe16621cacde518db13e5e1684d
MY <ruany@golive.cc>
2021-08-23 11:39:08 +0800

    打包配置修改

--- document/.vuepress/enhanceApp.js
+++ document/.vuepress/enhanceApp.js
@@ -1,5 +1,5 @@
-// import ElementUI from 'element-ui';
-// import DoElement from 'do-element';
+import ElementUI from 'element-ui';
+import DoElement from '../../lib';
 import 'element-ui/lib/theme-chalk/index.css';
 import '../../lib/theme-chalk/index.css';
 import './public/font/iconfont.css';
@@ -7,19 +7,22 @@ import './public/font/iconfont.css';
 export default ({ Vue, router }) => {
 	Vue.mixin({
 		mounted() {
-			import('element-ui').then(function (E) {
-				Vue.use(E);
-			});
+			Vue.use(ElementUI);
+			Vue.use(DoElement);
+			// import('element-ui').then(function (E) {
+			// 	console.log('我在加载element-ui');
+			// 	Vue.use(E);
 
-			import('do-element').then(function (D) {
-				Vue.use(D);
-			});
+			// });
+			// import('../../lib').then(function (D) {
+			// 	Vue.use(D);
+			// });
 		},
 	});
 
 	router.beforeEach((to, from, next) => {
 		if (to.path === '/') {
-			next('/zh/component/container');
+			next('/zh/component/guide/package');
 		} else if (to.path === '/zh/component/') {
 			next('/zh/component/container');
 		} else if (to.path === '/zh/utils/') {
