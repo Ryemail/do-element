2f3c6e937e0b33538e8b6d9277418964d2bf2672
MY <ruany@golive.cc>
2021-08-31 14:08:27 +0800

    babel-loader 修改

--- build/webpack.component.js
+++ build/webpack.component.js
@@ -49,10 +49,6 @@ module.exports = {
 				exclude: /(node_modules|bower_components)/,
 				use: {
 					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins: ['@babel/plugin-transform-runtime'],
-					},
 				},
 			},
 
