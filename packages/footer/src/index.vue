3ab7bf00374a0eacaf477b4d761f9a78c4892160
MY <ruany@golive.cc>
2021-11-29 18:22:10 +0800

    更新footer组件

--- packages/footer/src/index.vue
+++ packages/footer/src/index.vue
@@ -1,7 +1,14 @@
 <template>
 	<footer class="d-footer">
 		青橄榄智慧服务平台 Copyright © 2015-{{ endDate }} Golive All Rights
-		Reserved 浙ICP备14020207号-4
+		Reserved
+		<a
+			class="d-beian-link"
+			target="_blank"
+			href="https://beian.miit.gov.cn"
+		>
+			浙ICP备14020207号-4
+		</a>
 	</footer>
 </template>
 
